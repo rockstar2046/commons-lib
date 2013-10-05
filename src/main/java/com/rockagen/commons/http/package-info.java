e9fc834a34b51b84a4c2b98d53e23e022f9c7c5b
AGEN <agen@rockagen.com>
2013-10-06 01:43:21 +0800

    bug fix

---
+++ src/main/java/com/rockagen/commons/http/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * <b>Some http commons functions packaging</b>
+ * <p>apache.commons.httpclient must required</p>
+ * <p>note: Dependency com.rockagen.commons.util</p>
+ * @author AGEN
+ * @since JDK1.6
+ */
+package com.rockagen.commons.http;
